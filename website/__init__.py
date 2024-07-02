75a531af6fc2fb247c7769851c2d2ba0e658af0a
Michael Tran <michael.tran387@gmail.com>
2024-07-02 18:56:12 -0500

    save 2

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,29 @@
 from flask import Flask
+import sqlite3
 
 
 def create_app():
     app = Flask(__name__)
-    # app.config["SECRET_KEY"] = "supersecretkey"
+    app.config["SECRET_KEY"] = "supersecretkey"
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    conn = sqlite3.connect("new-new-music-tracker.db")
+    c = conn.cursor()
+    check = c.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name = 'users'"
+    )
+    if len(*check) == 0:
+        c.execute(
+            """
+        CREATE TABLE users (
+        username text
+        password text
+                )"""
+        )
+
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
+
 
 auth = Blueprint("auth", __name__)
+
+
+@auth.route("sign-up", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("sign-up.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if password1 != password2:
+            flash("Passwords do not match.", category="error")
+            return render_template("sign-up.html")
+
+        return render_template("sign-up.html")
+
+
+@auth.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@auth.route("/logout")
+def logout():
+    return render_template("index.html")
