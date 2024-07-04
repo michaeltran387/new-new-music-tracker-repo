abdab872818f2dbab9f44685a8fcf3cab93263de
Michael Tran <michael.tran387@gmail.com>
2024-07-04 16:57:19 -0500

    save 4

--- website/__init__.py
+++ website/__init__.py
@@ -12,18 +12,19 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    conn = sqlite3.connect("new-new-music-tracker.db")
-    c = conn.cursor()
-    check = c.execute(
+    con = sqlite3.connect("new-new-music-tracker.db")
+    cur = con.cursor()
+    res = cur.execute(
         "SELECT name FROM sqlite_master WHERE type='table' AND name = 'users'"
     )
-    if len(*check) == 0:
-        c.execute(
+    if len(res.fetchall()) == 0:
+        cur.execute(
             """
         CREATE TABLE users (
-        username text
-        password text
+        username text,
+        password blob
                 )"""
         )
+    con.close()
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, request, flash
 import bcrypt
+import sqlite3
+
+con = sqlite3.connect("new-new-music-tracker.db", check_same_thread=False)
+cur = con.cursor()
 
 
 auth = Blueprint("auth", __name__)
@@ -17,6 +21,21 @@ def signup():
         if password1 != password2:
             flash("Passwords do not match.", category="error")
             return render_template("sign-up.html")
+
+        # sqlstatement = "SELECT EXISTS(SELECT 1 from users WHERE username={username})".format(username=username)
+
+        password = b"super super secret password"
+        hashedpw = bcrypt.hashpw(password, bcrypt.gensalt())
+        check = "INSERT INTO users (username, password) VALUES ('{username}', '{password}')".format(
+            username=username, password="hashedpw"
+        )
+        print(check)
+        # print(hashedpw[])
+        cur.execute(check)
+        con.commit()
+        res = cur.execute("SELECT username FROM users")
+        res.fetchall()
+        print(res.fetchall())
 
         return render_template("sign-up.html")
 
